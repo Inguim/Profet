d305b3d84922e6a40a8c6a68bb065ee9f4b6224a
gmantovanibrito <gmantovanibrito@gmail.com>
2020-11-11 11:19:54 -0300

    Layouts e Tela Inicial

--- resources/views/layouts/noticias.blade.php
+++ resources/views/layouts/noticias.blade.php
@@ -10,6 +10,7 @@
    width: 100%;
    height: 20px;
    background-color: #086BAB;
+
 }
 </style>
 <head>
@@ -33,7 +34,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md" style="background: #086BAB;">
+        <nav class="navbar navbar-expand-md" style="background: #086BAB">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="logo3.png" style="height: 50px;">
@@ -100,9 +101,33 @@
                 </div>
             </div>
         </nav>
-            <main>
+        <div class="row">
+            <article class="col-2 bg-light mt-2" >
+                <div class="pt-2" id="noticias" style="margin-left: 5px">
+                    <h4 class=" text-center text-dark" style="margin-top:10px">Notícias</h4>
+                    <div id="noticias">
+                        <div class="card mb-1">
+                            <a class="card-link p-2" href="http://www.varginha.cefetmg.br/2019/08/19/inscricoes-para-a-15a-semana-ct-prorrogadas-ate-quarta-21/"> Inscrições para a 15ª Semana C&T prorrogadas até quarta (21) </a>
+                        </div>
+                        <div class="card mb-1">
+                            <a class="card-link p-2" href="http://www.meta.cefetmg.br/2019/06/03/esta-aberto-o-periodo-de-inscricoes-de-trabalhos-para-a-29a-meta/"> Está aberto o período de inscrições de trabalhos para a 29ª META </a>
+                        </div>
+                        <div class="card mb-1">
+                            <a class="card-link p-2 p-1" href="http://www.meta.cefetmg.br/2019/05/31/divulgado-o-edital-da-29a-mostra-especifica-de-trabalhos-e-aplicacoes-meta-do-cefet-mg/"> Divulgado o Edital da 29ª Mostra Específica de Trabalhos e Aplicações (META) do CEFET-MG </a>
+                        </div>
+                        <div class="card mb-1">
+                            <a class="card-link p-2 p-1" href="http://www.varginha.cefetmg.br/2019/08/23/semana-ct-recebe-propostas-de-coordenacoes-e-departamentos/"> Semana C&T recebe propostas de coordenações e departamentos </a>
+                        </div>
+                        <div class="card mb-1">
+                            <a class="card-link p-2 p-1" href="http://www.semanact.cefetmg.br/2019/09/13/trabalhos-aprovados-para-apresentacao-na-15a-semana-ct/"> Trabalhos aprovados para apresentação na 15ª Semana C&T </a>
+                        </div>
+                        <div class="card mb-1">
+                            <a class="card-link p-2 p-1" href="http://www.semanact.cefetmg.br/2019/10/09/disponivel-programacao-da-15a-semana-ct/"> Disponível Programação da 15ª Semana C&T </a>
+                        </div>
+                    </div>
+                <div>
+            </article>
                  @yield('content')
-            </main>
         </div>
     </div>
     <div class="footer"><p></p></div>
