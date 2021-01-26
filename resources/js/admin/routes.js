864e97b790bbe526f3b9ac2fd9619cf401914b9d
Inguim <igorazevedost@gmail.com>
2021-01-26 13:05:25 -0300

    Administrativa: Visualizar  Projeto | Layout

--- resources/js/admin/routes.js
+++ resources/js/admin/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Noticias from "./pages/Noticias";
 import Membros from "./pages/Membros";
 import Projetos from "./pages/Projetos";
+import VisualizarProjeto from "./pages/VisualizarProjeto";
 
 const Routes = () => {
   const route = process.env.MIX_APP_ROUTE;
@@ -11,6 +12,7 @@ const Routes = () => {
       <Route exact path={`${route}/administrativa/noticias`} component={Noticias} />
       <Route exact path={`${route}/administrativa/membros`} component={Membros} />
       <Route exact path={`${route}/administrativa/projetos`} component={Projetos} />
+      <Route exact path={`${route}/administrativa/projetos/:id`} component={VisualizarProjeto} />
     </Switch>
   );
 };
