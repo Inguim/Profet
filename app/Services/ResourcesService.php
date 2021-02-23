523dec0419ff164e87d25087f63012349f0469d8
Inguim <igorazevedost@gmail.com>
2021-02-22 21:10:04 -0300

    Inject: Noticia menu

--- app/Services/ResourcesService.php
+++ app/Services/ResourcesService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\Categoria;
 use App\Models\Curso;
 use App\Models\Menu;
+use App\Models\Noticia;
 use App\Models\Serie;
 
 class ResourcesService
@@ -27,5 +28,10 @@ class ResourcesService
     public function cursos()
     {
         return Curso::all();
+    }
+
+    public function noticias()
+    {
+        return Noticia::take(5)->get();
     }
 }
--- resources/views/layouts/noticias.blade.php
+++ resources/views/layouts/noticias.blade.php
@@ -35,30 +35,17 @@
     <div id="app">
         @component("components.navbar")
         @endcomponent
+        @inject('resources','App\Services\ResourcesService')
         <div class="row">
             <article class="col-2" >
                 <div class="pt-2" id="noticias" style="margin-left: 5px">
                     <h4 class=" text-center text-dark" style="margin-top:10px">Notícias</h4>
                     <div id="noticias">
+                    @foreach($resources->noticias() as $noticia)
                         <div class="card mb-1">
-                            <a class="card-link p-2" href="http://www.varginha.cefetmg.br/2019/08/19/inscricoes-para-a-15a-semana-ct-prorrogadas-ate-quarta-21/"> Inscrições para a 15ª Semana C&T prorrogadas até quarta (21) </a>
+                            <a class="card-link p-2" href="{{ $noticia->link }}">{{ $noticia->nome }}</a>
                         </div>
-                        <div class="card mb-1">
-                            <a class="card-link p-2" href="http://www.meta.cefetmg.br/2019/06/03/esta-aberto-o-periodo-de-inscricoes-de-trabalhos-para-a-29a-meta/"> Está aberto o período de inscrições de trabalhos para a 29ª META </a>
-                        </div>
-                        <div class="card mb-1">
-                            <a class="card-link p-2 p-1" href="http://www.meta.cefetmg.br/2019/05/31/divulgado-o-edital-da-29a-mostra-especifica-de-trabalhos-e-aplicacoes-meta-do-cefet-mg/"> Divulgado o Edital da 29ª Mostra Específica de Trabalhos e Aplicações (META) do CEFET-MG </a>
-                        </div>
-                        <div class="card mb-1">
-                            <a class="card-link p-2 p-1" href="http://www.varginha.cefetmg.br/2019/08/23/semana-ct-recebe-propostas-de-coordenacoes-e-departamentos/"> Semana C&T recebe propostas de coordenações e departamentos </a>
-                        </div>
-                        <div class="card mb-1">
-                            <a class="card-link p-2 p-1" href="http://www.semanact.cefetmg.br/2019/09/13/trabalhos-aprovados-para-apresentacao-na-15a-semana-ct/"> Trabalhos aprovados para apresentação na 15ª Semana C&T </a>
-                        </div>
-                        <div class="card mb-1">
-                            <a class="card-link p-2 p-1" href="http://www.semanact.cefetmg.br/2019/10/09/disponivel-programacao-da-15a-semana-ct/"> Disponível Programação da 15ª Semana C&T </a>
-                        </div>
-                    </div>
+                    @endforeach
                 <div>
             </article>
                  @yield('content')
