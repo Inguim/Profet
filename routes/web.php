9895406910ad6e3ad049e276bafc2b1d2aba3370
Inguim <igorazevedost@gmail.com>
2021-03-23 22:06:45 -0300

    Remoção de controllers desnecessários

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\ProjetoController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -42,6 +43,6 @@ Route::group(['middleware' => 'auth:web'], function () {
     Route::get('/projeto/novo', [ProjetoController::class, 'create'])->name('novoprojeto');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::resource('categoria', CategoriaController::class);
 
