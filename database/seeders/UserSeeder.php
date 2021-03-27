23d22f3b7b62ea11c018b00878fd77be9633a13c
Inguim <igorazevedost@gmail.com>
2021-03-26 21:05:19 -0300

    Seeder: User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Aluno;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,13 +16,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        $user = User::create([
             'name' => 'Profet',
             'email' => 'profet@gmail.com',
             'tipo' => 'aluno',
             'admin' => true,
             'status' => 'aprovado',
             'password' => Hash::make('profeT@9102')
+        ]);
+
+        Aluno::create([
+            'user_id' => $user->id,
+            'serie_id' => 3,
+            'curso_id' => 1
         ]);
     }
 }
