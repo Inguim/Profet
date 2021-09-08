141796443db1bce79d4ab63ff306fc6c8dc64bb7
profet <admin@profet.cefetvga.pro.br>
2021-09-08 13:28:00 -0300

    Maxlength noticias

--- resources/js/admin/pages/Noticias/index.js
+++ resources/js/admin/pages/Noticias/index.js
@@ -74,7 +74,10 @@ const Noticias = () => {
                 <h1>Inserir notícias:</h1>
                 <input type="hidden" name="id" value={id} required ref={register} />
                 <label htmlFor="nome">Titulo:</label>
-                <input type="text" id="nome" name="nome" maxlength="50" required ref={register({ required: true })} />
+                <input type="text" id="nome" name="nome" placeholder="Tamanho máximo de 50 caracteres" required ref={register({
+                                required: { value: true },
+                                maxLength: { value: 50, message: "Tamanho máximo de 50 caracteres!" }
+                            })} />
                 <label htmlFor="link">Link da notícia:</label>
                 <input type="text" id="link" name="link" required ref={register({ required: true })} />
                 <Button type="submit">Enviar</Button>
