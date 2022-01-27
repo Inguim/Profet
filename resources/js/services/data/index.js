812bdf14bb05c17c5308eae71faed3f30484004d
Inguim <igorazevedost@gmail.com>
2022-01-27 17:35:51 -0300

    React Services: Membros | (search)

--- resources/js/services/data/index.js
+++ resources/js/services/data/index.js
@@ -1,3 +1,4 @@
 export { default as apiNoticias } from './Noticias';
 export { default as apiUsuarios } from './Usuarios';
 export { default as apiProjetos } from './Projetos';
+export { default as apiMembros } from './Membros';
