1b38c5afdbd25e784499c5bd136e4137023ff4a6
Inguim <igorazevedost@gmail.com>
2022-02-04 10:59:15 -0300

    react Services: Solicitações

--- resources/js/react/services/data/index.js
+++ resources/js/react/services/data/index.js
@@ -3,3 +3,4 @@ export { default as apiUsuarios } from './Usuarios';
 export { default as apiFormProjetos } from './FormProjetos';
 export { default as apiProjetos } from './Projetos';
 export { default as apiMembros } from './Membros';
+export { default as apiSolicitacao } from './Solicitacao';
