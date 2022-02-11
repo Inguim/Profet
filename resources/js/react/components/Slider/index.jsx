938a5b81b6a1891f5ea36a8596f124c9569dc33b
Inguim <igorazevedost@gmail.com>
2022-02-11 15:56:50 -0300

    React Component: Slider | Slider funcional por overflow

--- resources/js/react/components/Slider/index.jsx
+++ resources/js/react/components/Slider/index.jsx
@@ -1,12 +1,43 @@
-import React from "react";;
+import React, { useEffect, useRef, useState } from "react";;
 import { ButtonSlide, Slide } from "./styles";
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
-const Slider = ({ children }) => {
+const Slider = ({ children, step = 220 }) => {
+  const [ toStep, setToStep ] = useState(0);
+
+  const container = useRef(null);
+
+  const handleStep = (to, step) => {
+    switch (to) {
+      case 1:
+        // var x = toStep + Math.round(window.innerWidth / 2);
+        // if(x > 0){
+        //     x = 0;
+        // }
+        // setToStep(x);
+        break;
+
+      case 2:
+        // var x = toStep - Math.round(window.innerWidth / 2);
+        // var listW = 8 * 220;
+        // if(window.innerWidth - listW > x)
+        // {
+        //     x = (window.innerWidth - listW) - 60;
+        // }
+        // setToStep(x < 0 ? x * -1 : x);
+        break;
+    }
+  };
+
   return (
-    <Slide>
-      <ButtonSlide>{"<"}</ButtonSlide>
+    <Slide ref={container} step={toStep} >
+      <ButtonSlide first direction={'to right'} onClick={() => handleStep(1, step)}>
+        <IoIosArrowBack  size={30} />
+      </ButtonSlide>
         {children}
-      <ButtonSlide>{">"}</ButtonSlide>
+      <ButtonSlide direction={'to left'} onClick={() => handleStep(2, step)}>
+        <IoIosArrowForward  size={30} />
+      </ButtonSlide>
     </Slide>
   )
 }
--- resources/js/react/components/Slider/styles.js
+++ resources/js/react/components/Slider/styles.js
@@ -2,10 +2,29 @@ import styled from "styled-components";
 import { Box } from "../CardSlider/styled";
 
 export const ButtonSlide = styled.button`
-  height: 290px;
+  height: 355px;
+  width: 30px;
   position: absolute;
   z-index: 1;
   width: 20px;
+  background-image: ${({ direction }) => `linear-gradient( ${direction},  rgba(53,114,149,0.5074404761904762) 0%, rgba(119,147,161,0.45) 21%, rgba(99,183,191,0.14) 56%, rgba(8,183,191,0) 100%  )`};
+  border: none;
+  left: ${({ first }) => first ? `0px`: ''};
+  opacity: 0.5;
+  transition: opacity ease-in-out 0.4s;
+
+  svg {
+    margin: 0;
+    padding: 0;
+    position: relative;
+    left: -12px;
+    z-index: +1;
+    fill: #327da8;
+  }
+
+  :hover {
+    opacity: 1.5;
+  }
 `;
 
 export const Slide = styled.div`
@@ -13,16 +32,22 @@ export const Slide = styled.div`
   max-width: 100vw;
   width: 100%;
   overflow-x: auto;
-  ::-webkit-scrollbar {
-    width: 0px;
-  }
+  transition: all ease-in-out 0.4s;
+  padding-left: ${({ step }) => step + "px"};
 
-  ${ButtonSlide} + ${Box} {
-    margin-left: 25px;
+  ::-webkit-scrollbar {
+    width: 0;
   }
 
   ${Box} + ${ButtonSlide} {
     right: 0;
+    right: -3px;
+  }
+
+  :hover {
+    ${ButtonSlide} {
+      opacity: 0.8;
+    }
   }
 `;
 
