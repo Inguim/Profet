10750a6954f6dd6b56aa8fda79d2b66b4e9ecbcf
Inguim <igorazevedost@gmail.com>
2022-02-14 09:57:38 -0300

    Run Prod

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 cd /home/profetcefetvga/profet
 php artisan storage:link
 composer install && yarn install && yarn prod
+php artisan migrate
+php artisan bb:seed --class=TiposNotificacaoSeeder
 # yarn prod
 
 # php artisan migrate && php artisan db:seed
